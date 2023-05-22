79f457c60dcfc7c4d88d5ece9cd57ff2d740119f
2022year <114775383+2022year@users.noreply.github.com>
2023-05-22 15:05:04 +0400

    Add files via upload

---
+++ sign/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import BaseRegisterView, LoginViewPage, PersonalPage, make_author, subscriptions_update
+from django.shortcuts import render
+
+
+urlpatterns = [
+    path('login/', LoginViewPage.as_view(), name='login'),
+    path('signup/', BaseRegisterView.as_view(), name='signup'),
+    path('upgrade/', make_author, name='upgrade'),
+    path('personal/', PersonalPage.as_view(), name='personal'),
+    path('subscriptions_update/', subscriptions_update, name='subscriptions_update'),
+]
